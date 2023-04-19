17a41d4434eadb4e17b81af21e5b79d2205d8443
MaksimZotov <zotovm256@gmail.com>
2023-04-19 20:53:53 +0300

    simplify printer text

--- lib/data/printer/printer_interactor.dart
+++ lib/data/printer/printer_interactor.dart
@@ -29,29 +29,7 @@ class PrinterInteractor {
   }
 
   void test(NetworkPrinter printer) {
-    printer.text(
-        'Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
-    printer.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ',
-        styles: PosStyles(codeTable: 'CP1252'));
-    printer.text('Special 2: blåbærgrød',
-        styles: PosStyles(codeTable: 'CP1252'));
-
-    printer.text('Bold text', styles: PosStyles(bold: true));
-    printer.text('Reverse text', styles: PosStyles(reverse: true));
-    printer.text('Underlined text',
-        styles: PosStyles(underline: true), linesAfter: 1);
-    printer.text('Align left', styles: PosStyles(align: PosAlign.left));
-    printer.text('Align center', styles: PosStyles(align: PosAlign.center));
-    printer.text('Align right',
-        styles: PosStyles(align: PosAlign.right), linesAfter: 1);
-
-    printer.text('Text size 200%',
-        styles: PosStyles(
-          height: PosTextSize.size2,
-          width: PosTextSize.size2,
-        ));
-
-    printer.feed(2);
+    printer.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
     printer.cut();
   }
 }
